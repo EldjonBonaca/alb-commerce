4a5146f71e84054196e144abebd53dcc20a379e6
EldjonBonaca <eldjon.bn@gmail.com>
2025-03-15 15:29:03 +0100

    pagination dashboard

--- app/(back-office)/dashboard/page.jsx
+++ app/(back-office)/dashboard/page.jsx
@@ -3,6 +3,7 @@ import Headings from '@/components/backoffice/Headings'
 import LargeCards from '@/components/backoffice/LargeCards'
 import SmallCards from '@/components/backoffice/SmallCards'
 import DashboardCharts from '@/components/backoffice/DashboardCharts'
+import CustomDataTable from '@/components/backoffice/CustomDataTable'
 
 export default function Dashboard() {
   return (
@@ -15,6 +16,7 @@ export default function Dashboard() {
       {/*Charts*/}
       <DashboardCharts/>
       {/*recent Orders*/}
+      <CustomDataTable/>
     </div>
   )
 }
--- components/backoffice/Navbar.jsx
+++ components/backoffice/Navbar.jsx
@@ -3,14 +3,19 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='flex items-center justify-between bg-slate-800 text-slate-50 h-16 px-8 py-4 fixed top-0 w-full left-60'>
+    <div className='flex items-center justify-between bg-slate-800 text-slate-50 h-20 py-8 fixed top-0 w-full px-8 z-50'>
         {/*Icon*/}
         <button><AlignJustify/></button>
         {/*3 Icons*/}
-        <div className="flex">
-            <button><Sun/></button>
-            <button><Bell/></button>
-            <button><User/></button>
+        <div className="flex space-x-3">
+            <button><Sun className='text-green-600'/></button>
+            
+            <button type="button" className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-transparent rounded-lg  ">
+              <Bell className='text-green-600'/>
+              <span className="sr-only">Notifications</span>
+                <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full -top-0 end-5 dark:border-gray-900">20</div>
+            </button>
+            <button><User className='text-green-600'/></button>
         </div>
     </div>
   )
